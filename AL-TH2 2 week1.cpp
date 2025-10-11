03b1fef2c338a99f3a9411fc5b858e284a0d6aeb
GyalGyal <siyul13gm@gmail.com>
2025-10-11 14:50:12 +0900

    Add w1

---
+++ AL-TH2 2 week1.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+/*
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	
+	int d[1000];
+
+		d[1] = 1;
+		d[2] = 2;
+		d[3] = 4;
+
+
+	for (int i = 4; i <= n; i++)
+	{
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+	
+	}
+	printf("%d", d[n]);
+}
+
+*/
+
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	int p[10000];
+	for (int i = 0; i <= n; i++)
+	{
+		scanf("%d", &p[i]);
+	}
+
+
+	for (int i = 5; i <= n; i++)
+	{
+		p[i] = p[i - 1] + p[i - 2] + p[i - 3] + p[i - 4];
+	}
+
+	printf("%d", p[n]);
+}
